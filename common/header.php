0d2ce1b24ae8d1584183d5d5b5a302149289a9c6
John Espitia <jespitiasa@outlook.com>
2023-05-19 14:01:13 -0500

    Add styles + remove unused code

---
+++ common/header.php
@@ -0,0 +1,44 @@
+<header class="">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <picture>
+                    <img src="resources/img/logo/logo-satena/original/logo-satena-original.svg" alt="" height="auto" width="80%">
+                </picture>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#top">Inicio
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">Destinos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.html">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Check-in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="one-page.html">Comprobante de Compra</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="one-page.html">Transparencia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="one-page.html">Atención al Ciudadano</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="one-page.html">Participa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
